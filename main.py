1e0da8ba5a89b0cc9a3482aaaf7899c9670ee84d
Skieg <ifjvaradia@gmail.com>
2025-02-02 13:44:28 +0100

    Added player + circleshape + Self.radius in constants, player added in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -7,13 +8,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    frame = pygame.time.Clock()
+    dt = 0 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-        
+        dt = frame.tick(60) / 1000
+
+    
 
 
 if __name__ == "__main__":
